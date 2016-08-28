9d00de550720396403f386cfe7bab7b59132c6a4
drkitty <bumblingkitty@gmail.com>
2016-08-27 22:40:46 -0700

    Update 'maia' keymap to revision 6

--- keyboards/planck/keymaps/maia/keymap.c
+++ keyboards/planck/keymaps/maia/keymap.c
@@ -4,131 +4,151 @@
 extern keymap_config_t keymap_config;
 
 // Layer names //
-#define BAS 0
-#define QWE 1
-#define WRK 2
-#define NUM 3
-#define SYM 4
-#define NAV 5
+enum {
+    NON = 0,
+    Q_W,
+    W_W,
+    Q_L,
+    W_L,
+    NUM,
+    NAV,
+};
 
 // Macros to make the keymap more visually appealing //
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-/*#define LY(layer) ACTION_FUNCTION(layer)*/
-#define LY(layer) TO((layer), ON_PRESS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BAS] = {
+  [NON] = {
     {
-      KC_TAB , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT ,
+      _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,
     },
     {
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,
     },
     {
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+      _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,
     },
     {
-      KC_ESC , MO(NAV), KC_LALT, KC_LGUI, MO(NUM), KC_SPC ,
-      KC_BSPC, MO(SYM), KC_RGUI, KC_RALT, LY(QWE), LY(WRK),
+      _______, MO(NAV), KC_LALT, KC_LGUI, MO(NUM), KC_SPC ,
+      KC_BSPC, MO(NUM), KC_RGUI, KC_RALT, MO(NAV), XXXXXXX,
     },
   },
 
-  [QWE] = {
+  [Q_W] = {
     {
-      _______, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,
-      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , _______,
+      KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,
+      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_ENT ,
     },
     {
-      _______, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,
+      KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,
       KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     },
     {
-      _______, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,
-      KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
+      KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,
+      KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
     },
     {
-      _______, _______, _______, _______, _______, _______,
+      KC_LCTL, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,
     },
   },
 
-  [WRK] = {
+  [W_W] = {
     {
-      _______, KC_Q   , KC_D   , KC_R   , KC_W   , KC_B   ,
-      KC_J   , KC_F   , KC_U   , KC_P   , KC_SCLN, _______,
+      KC_TAB , KC_Q   , KC_D   , KC_R   , KC_W   , KC_B   ,
+      KC_J   , KC_F   , KC_U   , KC_P   , KC_SCLN, KC_ENT ,
     },
     {
-      _______, KC_A   , KC_S   , KC_H   , KC_T   , KC_G   ,
+      KC_CAPS, KC_A   , KC_S   , KC_H   , KC_T   , KC_G   ,
       KC_Y   , KC_N   , KC_E   , KC_O   , KC_I   , KC_QUOT,
     },
     {
-      _______, KC_Z   , KC_X   , KC_M   , KC_C   , KC_V   ,
-      KC_K   , KC_L   , KC_COMM, KC_DOT , KC_SLSH, _______,
+      KC_LSFT, KC_Z   , KC_X   , KC_M   , KC_C   , KC_V   ,
+      KC_K   , KC_L   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
     },
     {
+      KC_LCTL, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,
+    },
+  },
+
+  [Q_L] = {
+    {
+      KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,
+      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_ENT ,
+    },
+    {
+      KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,
+      KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    },
+    {
+      KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,
+      KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+    },
+    {
+      KC_ESC , _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,
+    },
+  },
+
+  [W_L] = {
+    {
+      KC_TAB , KC_Q   , KC_D   , KC_R   , KC_W   , KC_B   ,
+      KC_J   , KC_F   , KC_U   , KC_P   , KC_SCLN, KC_ENT ,
+    },
+    {
+      KC_LCTL, KC_A   , KC_S   , KC_H   , KC_T   , KC_G   ,
+      KC_Y   , KC_N   , KC_E   , KC_O   , KC_I   , KC_QUOT,
+    },
+    {
+      KC_LSFT, KC_Z   , KC_X   , KC_M   , KC_C   , KC_V   ,
+      KC_K   , KC_L   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+    },
+    {
+      KC_ESC , _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,
     },
   },
 
   [NUM] = {
     {
-      _______, XXXXXXX, KC_TILD, KC_GRV , XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_MINS, KC_EQL , KC_PLUS, KC_UNDS, _______,
+      _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,
+      KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL , _______,
     },
     {
       _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,
       KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_ESC ,
     },
     {
-      XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
-      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , XXXXXXX,
+      _______, KC_GRV , KC_TILD, KC_LPRN, KC_RPRN, KC_LCBR,
+      KC_RCBR, KC_LBRC, KC_RBRC, KC_UNDS, KC_PLUS, _______,
     },
     {
       _______, XXXXXXX, _______, _______, _______, _______,
-      _______, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,
-    },
-  },
-
-  [SYM] = {
-    {
-      _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, XXXXXXX,
-      XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, _______,
-    },
-    {
-      _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,
-      KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_PIPE, XXXXXXX,
-    },
-    {
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    },
-    {
-      _______, XXXXXXX, _______, _______, XXXXXXX, _______,
       _______, _______, _______, _______, XXXXXXX, XXXXXXX,
     },
   },
 
   [NAV] = {
     {
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, DF(Q_W), DF(W_W), DF(Q_L), DF(W_L), XXXXXXX,
       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
     },
     {
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,
       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
     },
     {
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, _______,
+      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
+      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
     },
     {
       _______, _______, _______, _______, XXXXXXX, _______,
-      _______, XXXXXXX, _______, _______, XXXXXXX, RESET  ,
+      _______, XXXXXXX, _______, _______, _______, RESET  ,
     },
   },
 };
