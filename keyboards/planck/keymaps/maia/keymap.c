41a5c9b12b4e8c9763414ff14c88f99c7cc29e4c
drkitty <bumblingkitty@gmail.com>
2017-05-27 21:37:21 -0700

    I honestly don't know

--- keyboards/planck/keymaps/maia/keymap.c
+++ keyboards/planck/keymaps/maia/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     },
     {
       _______, MO(NAV), KC_LALT, KC_LGUI, MO(NUM), KC_SPC ,
-      KC_BSPC, MO(NUM), KC_RGUI, KC_RALT, MO(NAV), XXXXXXX,
+      KC_BSPC, MO(NUM), KC_RGUI, KC_RALT, MO(NAV), KC_DEL ,
     },
   },
 
