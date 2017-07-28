1d98b396e1d62e94951ab29ae4b47f50c0fd0198
drkitty <bumblingkitty@gmail.com>
2017-07-27 18:05:24 -0700

    Move keys around yet again

--- keyboards/planck/keymaps/maia/keymap.c
+++ keyboards/planck/keymaps/maia/keymap.c
@@ -23,12 +23,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_J   , KC_F   , KC_U   , KC_P   , KC_QUOT, KC_ENT ,
     },
     {
-      KC_LCTL, KC_A   , KC_S   , KC_H   , KC_T   , KC_G   ,
-      KC_Y   , KC_N   , KC_E   , KC_O   , KC_I   , KC_RCTL,
+      MO(SYM), KC_A   , KC_S   , KC_H   , KC_T   , KC_G   ,
+      KC_Y   , KC_N   , KC_E   , KC_O   , KC_I   , MO(SYM),
     },
     {
-      MO(SYM), KC_Z   , KC_X   , KC_M   , KC_C   , KC_V   ,
-      KC_K   , KC_L   , KC_COMM, KC_DOT , KC_SCLN, MO(SYM),
+      KC_LCTL, KC_Z   , KC_X   , KC_M   , KC_C   , KC_V   ,
+      KC_K   , KC_L   , KC_COMM, KC_DOT , KC_SCLN, KC_RCTL,
     },
     {
       KC_ESC , MO(NAV), KC_LALT, KC_LGUI, KC_LSFT, KC_SPC ,
@@ -58,15 +58,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SYM] = {
     {
       _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,
-      KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL , _______,
+      KC_CIRC, KC_7   , KC_8   , KC_9   , KC_MINS, _______,
     },
     {
-      _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,
-      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
+      _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,
+      KC_ASTR, KC_4   , KC_5   , KC_6   , KC_PLUS, _______,
     },
     {
-      _______, KC_SLSH, KC_QUES, KC_LPRN, KC_RPRN, KC_LCBR,
-      KC_RCBR, KC_LBRC, KC_RBRC, KC_UNDS, KC_PLUS, _______,
+      _______, KC_SLSH, KC_QUES, KC_LCBR, KC_RCBR, KC_UNDS,
+      KC_EQL , KC_1   , KC_2   , KC_3   , KC_0   , _______,
     },
     {
       _______, XXXXXXX, _______, _______, _______, _______,
@@ -80,12 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       DF(NON), KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
     },
     {
-      _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,
-      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
+      XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,
+      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
     },
     {
-      XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
-      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , XXXXXXX,
+      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
+      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
     },
     {
       _______, MO(NAV), _______, _______, _______, _______,
